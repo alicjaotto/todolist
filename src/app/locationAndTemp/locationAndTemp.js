f282752f12ada0a3deab576b73249efb93393d7c
Ala Otto <alicja.otto@gmail.com>
2018-11-05 21:45:41 +0100

    location and weather service works!

--- src/app/locationAndTemp/locationAndTemp.js
+++ src/app/locationAndTemp/locationAndTemp.js
@@ -14,26 +14,26 @@
 		function locationAndTempController($scope, $http) {
 			if (navigator.geolocation) {
 				navigator.geolocation.getCurrentPosition(function(position) {
-					$scope.$apply(function(position){
-						var lat = position.coords.latitude;
-						var lon = position.coords.longitude;
-						getWeather(lat, lon);
-					});
+					var lat = position.coords.latitude;
+					var lon = position.coords.longitude;
+					getWeather(lat, lon);
 				});
 			} else {
-				console.log('Geolocation is not supported by this browser');
+				var locationError = document.getElementById('location-error');
+				var message = document.createTextNode('This browser does not support geolocation');
+				locationError.appendChild(message);
 			}
 
+//move to separate service
 			function getWeather(lat, lon) {
 				$http.get('https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&APPID=8f5da5a3c95b945d1b9119efc8bb164b')
-					.success(function(data){
-						var weatherData = JSON.parse(JSON.stringify(data));
+					.then(function(data){
+						var weatherData = JSON.parse(JSON.stringify(data.data));
 						$scope.location = weatherData.name;
-						$scope.temp = Math.round(weatherData.main.temp)
-					})
-					.error(function(err){
-						console.log('Error retrieving markets');
-					});
+						$scope.temp = Math.round((weatherData.main.temp - 32) / 1.8);
+					}, (function(err){
+						console.log('Error retrieving weather data');
+					}));
 			}
 		}
 })(window.angular);
